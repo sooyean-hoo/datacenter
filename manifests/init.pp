c5b9e0a4a801cb7fa5e27339c7891f1cb3e4f4af
Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>
2020-07-09 19:28:28 +0800

    lint-ed

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,25 +5,24 @@
 # @example
 #   include datacenter
 class datacenter (
-  String $datacenterName = '?' ,
+  String $datacenter_name = '?' ,
 ){
 # OS-specific
 #  $datacenter = 'AWS';
-  
-  if $datacenterName == '?'  {
+  if $datacenter_name == '?'  {
 
     if  $facts[ 'domain' ] == 'platform9.puppet.net'  {
-      $datacenterName_ = 'P9'
+      $datacenter_name_ = 'P9'
     }elsif   $facts[ 'ec2_metadata' ] and
       $facts[ 'ec2_metadata' ]['services'] and
       $facts[ 'ec2_metadata' ]['services']['domain'] == 'amazonaws.com'  {
-        $datacenterName_ = "AWS"  
+        $datacenter_name_ = 'AWS'
     }else {
-      $datacenterName_ = "UKNOWN"
+      $datacenter_name_ = 'UKNOWN'
     }
   }
 
-  if $datacenterName == ''  {
+  if $datacenter_name == ''  {
       case $facts[ 'kernel' ] {
         'windows': {
           file { 'datacenter.txt':
@@ -36,59 +35,59 @@ class datacenter (
           ensure => 'absent',
           path   => '/etc/puppetlabs/facter/facts.d/datacenter.txt',
         }
-      } 
+      }
         default: {
           fail('Unsupported operating system!')
         }
     }
-  
   } else {
-     
-    unless  $datacenterName_  {
-      $datacenterName_ = $datacenterName  
+
+    unless  $datacenter_name_  {
+      $datacenter_name_ = $datacenter_name
     }
-      case $facts['kernel'] {
-        'windows': {
-          file { 'datacenter.txt':
-          ensure => 'file',
-          path   => 'C:\\ProgramData\\PuppetLabs\\facter\\facts.d\\datacenter.txt',
-          content => "datacenter=$datacenterName_",
-          require => File['fact.d'] ,
-        }
-        file { 'fact.d':
-          ensure => 'directory',
-          path   => 'C:\\ProgramData\\PuppetLabs\\facter\\facts.d',
-          recurse => true,
-        }
-        }
-        'Linux': {
-          file { 'datacenter.txt':
-          ensure => 'file',
-          path   => '/etc/puppetlabs/facter/facts.d/datacenter.txt',
-          content => "datacenter=$datacenterName_",
-          require => File['fact_d'] ,
-        }
-          file { 'fact_d':
-          ensure => 'directory',
-          path   => '/etc/puppetlabs/facter/facts.d/',
-          recurse => true,
-          require => File['facter'] ,
-        }
-        file { 'facter':
-          ensure => 'directory',
-          path   => '/etc/puppetlabs/facter',
-          recurse => true,
-          require => File['puppetlabs'] ,
-        }
-        file { 'puppetlabs':
-          ensure => 'directory',
-          path   => '/etc/puppetlabs',
-          recurse => true,
-        }
-        }
-        default: {
-          fail('Unsupported operating system!')
-        }
-    }    
+
+    case $facts['kernel'] {
+      'windows': {
+        file { 'datacenter.txt':
+        ensure  => 'file',
+        path    => 'C:\\ProgramData\\PuppetLabs\\facter\\facts.d\\datacenter.txt',
+        content => "datacenter=${datacenter_name_}",
+        require => File['fact.d'] ,
+      }
+      file { 'fact.d':
+        ensure  => 'directory',
+        path    => 'C:\\ProgramData\\PuppetLabs\\facter\\facts.d',
+        recurse => true,
+      }
+      }
+      'Linux': {
+        file { 'datacenter.txt':
+        ensure  => 'file',
+        path    => '/etc/puppetlabs/facter/facts.d/datacenter.txt',
+        content => "datacenter=${datacenter_name_}",
+        require => File['fact_d'] ,
+      }
+        file { 'fact_d':
+        ensure  => 'directory',
+        path    => '/etc/puppetlabs/facter/facts.d/',
+        recurse => true,
+        require => File['facter'] ,
+      }
+      file { 'facter':
+        ensure  => 'directory',
+        path    => '/etc/puppetlabs/facter',
+        recurse => true,
+        require => File['puppetlabs'] ,
+      }
+      file { 'puppetlabs':
+        ensure  => 'directory',
+        path    => '/etc/puppetlabs',
+        recurse => true,
+      }
+      }
+      default: {
+        fail('Unsupported operating system!')
+      }
     }
+  }
 }
