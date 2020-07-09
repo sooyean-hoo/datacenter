ae53cb7bb7931a88674126cf7399496f2b0fa115
Hoo Sooyean 何書淵 <Sooyean.hoo@puppet.com>
2020-07-10 03:07:58 +0800

    Add in default hiera data and  add in self-intro class

--- manifests/selfintro.pp
+++ manifests/selfintro.pp
@@ -16,7 +16,35 @@ class datacenter::selfintro {
     'password'
     ]
 
-  notify { $datacenter_keys:
-    message => " ${title}  = ${lookup($title)}",
+  $datacenter_values=[
+    'datacentername',
+    'datacenterurl',
+    'datacenteraddress',
+    'datacentertelno',
+    'datacentertechology',
+    'datacentermanagedby',
+    'motd',
+    'password'
+    ]
+
+  $bar=split('                      ','').join('========')
+    
+  notify { "0${$bar}": }
+  $datacenter_keys.each |String $hkey| {
+    $v =  lookup($hkey)
+    notify { " ${hkey} = ${v} ": }
+  }  
+  
+  notify { "1${$bar}": }
+  
+  $msg = ($datacenter_keys.map | $hkey | {   " ${hkey} :          ${lookup( $hkey ) }\r\n" } ).join
+  
+  notify { "Message\r\n  ${msg}" :  }
+  
+  notify { "2${$bar}": }
+
+  file{'/tmp/datacenterInfo.txt': 
+    content => $msg ,
   }
+
 }
